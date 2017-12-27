345c61211957a43d0385e9ac557e4c2ba08de98a
ac065455 <chen.anthony.1109@gmail.com>
2017-12-27 10:19:33 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//COMPONENTS
+import Routes from './components/routes';
+
+ReactDOM.render(<Routes/>, document.querySelector('#root'));
